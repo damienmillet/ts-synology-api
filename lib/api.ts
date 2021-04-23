0798bca666b8c17e73b6e1d99668120de0523dab
damienmillet <contact@damien-millet.dev>
2021-04-23 16:58:07 +0200

    🏷️ add generique response

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,13 @@
 import { stringify } from "querystring";
 
+export type response<T> = {
+  data?: T;
+  success: boolean;
+  error?: {
+    code: number;
+  };
+};
+
 const Api = {
   baseUrl: process.env.SYNOLOGY_URI,
   headers: new Headers({
