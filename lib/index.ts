a027b28e80637e4f445c1592fbf246de7bc6c5ba
damienmillet <contact@damien-millet.dev>
2021-04-24 16:14:12 +0200

    ⚡️ Add api's method

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,12 @@
 import Auth from "./api/auth";
 import Info from "./api/info";
 import NormalUser from "./core/normal-user";
+import Feed from "./download-station/rss/feed";
+import Site from "./download-station/rss/site";
+import Schedule from "./download-station/schedule";
+import Statistic from "./download-station/statistic";
 import Task from "./download-station/task";
+import InfoDS from "./download-station/info";
 
 const Syno = {
   Api: {
@@ -10,6 +15,13 @@ const Syno = {
   },
   Core: { NormalUser: NormalUser },
   DownloadStation: {
+    Info: InfoDS,
+    Rss: {
+      Feed: Feed,
+      Site: Site,
+    },
+    Schedule: Schedule,
+    Statistic: Statistic,
     Task: Task,
   },
 };
